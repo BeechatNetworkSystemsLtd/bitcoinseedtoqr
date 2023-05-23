d8f29a07fec974d6a1b919b43bb946934b75c296
Beechat Network Systems Ltd <7268708+BeechatNetworkSystemsLtd@users.noreply.github.com>
2023-05-23 18:25:35 +0100

    V1.0

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# bitcoinseedtoqr
-Seed to QR is a client-side tool for generating and encrypting Bitcoin wallet mnemonic seeds into QR codes, with a focus on ensuring security, privacy, and ease of use
+# Seed to QR Code Generator
+
+## Overview
+This project is a client-side tool for generating Bitcoin wallet mnemonic seeds and encrypting them into a QR code. The project adheres to strict security and privacy principles and ensures that the seed never leaves the user's device. 
+
+## Features
+- Generate a mnemonic seed for a Bitcoin wallet.
+- Generate a QR code for easy scanning and storage of the seed.
+- The seed is encrypted with AES before it is converted into a QR code for an extra layer of security.
+- Decrypt the QR code to retrieve the original mnemonic seed.
+- The mnemonic seed, address, and the QR codes can be easily copied to clipboard.
+- Start a QR code scanner.
+
+## How to Use
+1. Clone the repository and install all the required dependencies.
+2. Run the server.
+3. Open the tool in your browser.
+4. Use the 'Generate Encrypted QR Code' button to generate a mnemonic seed and its QR code.
+5. Use the 'Decrypt QR Code' button to decrypt an encrypted QR code and retrieve the original mnemonic seed.
+
+## Dependencies
+- html5-qrcode
+- CryptoJS
+- bip39
+- bitcoinjs-lib
+- qrcode
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Contributing
+Please read CONTRIBUTING.md for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Support
+If you have any issues or feature requests, please file issues and pull requests against this repo for quick response.
+
+## Acknowledgements
+We appreciate the support and contributions from the community that help make this project better.
---
+++ app/static/js/main.js
@@ -0,0 +1,139 @@
+const Html5QrcodeScanner = require('html5-qrcode').Html5QrcodeScanner;
+const CryptoJS = require('crypto-js');
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
+const QRCode = require('qrcode');
+
+function generateSeed() {
+    let mnemonic = bip39.generateMnemonic(128);
+    document.getElementById('mnemonic').value = mnemonic;
+
+    let seed = bip39.mnemonicToSeedSync(mnemonic);
+    let root = bitcoin.bip32.fromSeed(seed);
+    let path = "m/44'/0'/0'/0/0";
+    let keyPair = root.derivePath(path);
+    let { address } = bitcoin.payments.p2sh({
+        redeem: bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey }),
+        network: bitcoin.networks.bitcoin,
+    });
+
+    document.getElementById('address').value = address;
+
+
+    // Generate the mnemonic and address QR codes
+    QRCode.toCanvas(document.getElementById('mnemonic-qr'), mnemonic, { errorCorrectionLevel: 'H', scale: 6 }, function (error) {
+        if (error) console.error(error);
+        console.log('Mnemonic QR code successfully generated!');
+        document.getElementById('mnemonic-qr').parentNode.style.display = "block";  // Show the canvas container
+    });
+    QRCode.toCanvas(document.getElementById('address-qr'), address, { errorCorrectionLevel: 'H', scale: 6 }, function (error) {
+        if (error) console.error(error);
+        console.log('Address QR code successfully generated!');
+        document.getElementById('address-qr').parentNode.style.display = "block";  // Show the canvas container
+    });
+
+    return mnemonic;
+}
+
+
+document.getElementById('generate').addEventListener('click', () => {
+    let mnemonic = generateSeed();
+    let password = document.getElementById('password').value;
+    let ciphertext = CryptoJS.AES.encrypt(mnemonic, password).toString();
+
+    console.log('Ciphertext:', ciphertext);  // Log ciphertext to console
+
+    if (ciphertext) {
+        QRCode.toCanvas(document.getElementById('encrypted-qr'), ciphertext, { errorCorrectionLevel: 'H', scale: 6 }, function (error) {
+            if (error) console.error('Error generating QR code:', error);
+            else {
+                console.log('QR code successfully generated!');
+                document.getElementById('encrypted-qr').parentNode.style.display = "block";  // Show the canvas container
+            }
+        });
+
+        document.getElementById('ciphertext').value = ciphertext;  // Store ciphertext
+    } else {
+        console.error('No ciphertext was generated!');
+    }
+});
+
+function copyToClipboard(id) {
+    let copyText = document.getElementById(id);
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    if (!navigator.clipboard) {
+        try {
+            document.execCommand('copy');
+            alert('Copied to clipboard');
+        } catch (err) {
+            alert('Failed to copy text');
+        }
+    } else {
+        navigator.clipboard.writeText(copyText.value).then(function() {
+            alert('Copied to clipboard');
+        }, function(err) {
+            alert('Failed to copy text: ', err);
+        });
+    }
+}
+
+document.getElementById('copy-mnemonic').addEventListener('click', function() {
+    copyToClipboard('mnemonic');
+});
+
+document.getElementById('copy-address').addEventListener('click', function() {
+    copyToClipboard('address');
+});
+
+
+
+document.getElementById('decrypt').addEventListener('click', () => {
+    let ciphertext = document.getElementById('scanned-ciphertext').value;  // Get scanned ciphertext
+    let password = document.getElementById('password').value;
+    let bytes = CryptoJS.AES.decrypt(ciphertext, password);
+    let originalText = bytes.toString(CryptoJS.enc.Utf8);
+    document.getElementById('mnemonic').value = originalText;
+    console.log(originalText);
+
+    let seed = bip39.mnemonicToSeedSync(originalText);
+    let root = bitcoin.bip32.fromSeed(seed);
+    let path = "m/44'/0'/0'/0/0";
+    let keyPair = root.derivePath(path);
+    let { address } = bitcoin.payments.p2sh({
+        redeem: bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey }),
+        network: bitcoin.networks.bitcoin,
+    });
+
+    document.getElementById('address').value = address;
+
+    // Generate the mnemonic and address QR codes
+    QRCode.toCanvas(document.getElementById('mnemonic-qr'), originalText, { errorCorrectionLevel: 'H', scale: 6 }, function (error) {
+        if (error) console.error(error);
+        console.log('Mnemonic QR code successfully regenerated!');
+    });
+    QRCode.toCanvas(document.getElementById('address-qr'), address, { errorCorrectionLevel: 'H', scale: 6 }, function (error) {
+        if (error) console.error(error);
+        console.log('Address QR code successfully regenerated!');
+    });
+});
+
+
+
+
+let scanner = new Html5QrcodeScanner("scanner", { fps: 10, qrbox: 250 });
+document.getElementById('start-scanner').addEventListener('click', () => {
+    scanner.render(successCallback, errorCallback);
+});
+
+function successCallback(message) {
+    document.getElementById('scan-result').innerText = "QR code scanned successfully.";
+    document.getElementById('scanned-ciphertext').value = message;
+    scanner.clear();
+}
+
+
+function errorCallback(error) {
+    console.log(error);
+}
